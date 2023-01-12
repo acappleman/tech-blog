590a13b6b90c95227b2e311c5f2ccbd967805eed
acappleman <112197155+acappleman@users.noreply.github.com>
2023-01-12 12:18:07 -0500

    Working application to add blogs, login/logout, view blogs, and add comments and view comments

---
+++ seeds/seed.js
@@ -0,0 +1,39 @@
+const sequelize = require('../config/connection');
+const { User, Blog, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  const blogs = await Blog.findAll();
+  
+  console.log(blogs);
+  console.log(blogs[0].id)
+  console.log(commentData);
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      blog_id: blogs[Math.floor(Math.random() * blogs.length)].id,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
